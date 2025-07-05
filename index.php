c0f1ee43cd87724c9db3b4a413b45f9ec88a14c5
WILTHONBAQUE2005 <moreirajunior2501@gmail.com>
2025-07-05 14:45:32 -0500

    actualizacion

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
-// Autocarga manual
 require_once __DIR__ . '/config/Database.php';
 require_once __DIR__ . '/repositorios/MySQLProductRepository.php';
 require_once __DIR__ . '/servicios/ProductService.php';
 require_once __DIR__ . '/controlador/ProductController.php';
 
-// 1. Conexión y wiring
 $pdo      = Database::connect();
 $repo     = new MySQLProductRepository($pdo);
 $service  = new ProductService($repo);
 $ctrl     = new ProductController($service);
 
-// 2. Punto único de entrada: en este ejemplo, siempre listamos
 $ctrl->index();
